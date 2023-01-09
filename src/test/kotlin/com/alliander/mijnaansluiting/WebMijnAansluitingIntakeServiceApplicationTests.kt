cb59a82e28fc1682252712827e51537dde6c37f9
Laura Koekenberg <laura.koekenberg@alliander.com>
2023-01-09 11:44:22 +0100

    eerste setup

---
+++ src/test/kotlin/com/alliander/mijnaansluiting/WebMijnAansluitingIntakeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alliander.mijnaansluiting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebMijnAansluitingIntakeServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
